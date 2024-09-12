ffc8571a40d6882c860f1f3467a209b330d525a2
LebronzoCodes <alebronsfs@gmail.com>
2024-09-12 17:10:19 -0400

    AddedAFriend

--- My project (2)/Assets/Scripts/WASD_Tran.cs
+++ My project (2)/Assets/Scripts/WASD_Tran.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class WASD_Tran : MonoBehaviour
 {
+
+    //Variables
+
+    public float speed = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,36 @@ public class WASD_Tran : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Find position of the object every frame, then create a vector
+        Vector2 pos = transform.position;
+
+        //If I press 'D', move my position to the right
+        if(Input.GetKey(KeyCode.RightArrow)) 
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        //If I press 'A', move my position to the left
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        //If I press 'W', move my position upwards
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        //If I press 'S', move my position downwards
         
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        //What allows us to move our object relative to pos
+        transform.position = pos;
     }
 }
